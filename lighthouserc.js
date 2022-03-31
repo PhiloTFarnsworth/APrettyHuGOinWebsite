b6a465d9ca0d26e2ea3f8fdc8836900701af2d76
Michael Gardner <Mgardner3000@gmail.com>
2022-03-31 13:53:43 -0700

    Better developer server command, also should fix playwright testing

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
         url: ['http://localhost:1313/APrettyHuGoinWebsite/',
             'http://localhost:1313/APrettyHuGoinWebsite/diatribes/',
             'http://localhost:1313/APrettyHuGoinWebsite/diatribes/index/'],
-      startServerCommand: 'hugo server',
+      startServerCommand: 'hugo server --watch=false --quiet --minify',
       },
       upload: {
         target: 'temporary-public-storage',
